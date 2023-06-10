59c3de5e05e643d63b2c950c99bfbdfce3d2b2e6
Narayanbhat166 <bhatj1964@gmail.com>
2023-06-10 22:36:28 +0530

    docs: add documentation

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,21 @@
+/// This is the Terminal User Interface for a player (kitten ) to practice / play with other kittens
+///
+/// Every player is a kitten
+/// The Goal of a kitten is to sharpen his paws to fight with other kitties
+///
+/// Each client tries to connect to a server ( master Cat ) for communication
+/// The communication channel is web sockets
+///
+/// If the connection is successful, then game areana can be entered
+/// If connection fails, only practice section will be available
+///
+/// The Clients are always in sync about all the players currently online
+/// and available to play
+///
+/// To enter a game, a kitten has to challenge the other kitten
+/// Once the opponent kitten accepts the challenge, game begins
+///
+///
 use crossterm::{
     self,
     event::{DisableMouseCapture, EnableMouseCapture, KeyCode},
@@ -64,7 +82,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: Arc<Mutex<App>>) -> io::Result<()> {
-    let tick_rate = Duration::from_millis(100);
+    let tick_rate = Duration::from_millis(1000);
 
     let mut last_tick = std::time::Instant::now();
     loop {
@@ -75,6 +93,12 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: Arc<Mutex<App>>) -> io::
         let cloned_app = app.clone();
         terminal.draw(move |f| ui(f, cloned_app))?;
 
+        // Why use poll?
+        // Read is a blocking call untill the event is available
+        // The poll function will poll for the given time duration and
+        // if no input is available, then it will return Ok(false)
+        //
+        // So when is this useful?
         if crossterm::event::poll(timeout).unwrap() {
             if let crossterm::event::Event::Key(key_event) = crossterm::event::read()? {
                 if let KeyCode::Esc = key_event.code {
