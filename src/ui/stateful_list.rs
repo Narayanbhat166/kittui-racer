695b52090d8b3e8204303443131ea6aefabedb9a
Narayanbhat166 <bhatj1964@gmail.com>
2023-06-30 22:58:46 +0530

    fix: next and previous selection panicing

--- src/ui/stateful_list.rs
+++ src/ui/stateful_list.rs
@@ -31,32 +31,40 @@ impl<T> StatefulList<T> {
         self.items.push(item)
     }
 
+    /// Select the next item in the list, in cyclic manner
     pub fn next(&mut self) {
-        let i = match self.state.selected() {
-            Some(i) => {
-                if i >= self.items.len() - 1 {
-                    0
-                } else {
-                    i + 1
-                }
-            }
-            None => 0,
-        };
-        self.state.select(Some(i));
+        // Check if there are any elements in the list and then proceed
+        if !self.items.is_empty() {
+            let next_selection = self
+                .state
+                .selected()
+                .map(|selected_index| {
+                    // Check if selection is at the end of the list
+                    (selected_index + 1 == self.items.len())
+                        .then_some(0) // If so, set the selection back to index 0
+                        .unwrap_or(selected_index.saturating_add(1)) // else, increment the selection
+                })
+                .unwrap_or(0); // If no index is selected previously, select 0th index
+            self.state.select(Some(next_selection));
+        }
     }
 
+    /// Select the previous element in the list, in cyclic manner.
     pub fn previous(&mut self) {
-        let i = match self.state.selected() {
-            Some(i) => {
-                if i == 0 {
-                    self.items.len() - 1
-                } else {
-                    i - 1
-                }
-            }
-            None => 0,
-        };
-        self.state.select(Some(i));
+        // Check if there are any elements in the list and then proceed
+        if !self.items.is_empty() {
+            let select_previous = self
+                .state
+                .selected()
+                .map(|selected_index| {
+                    // Check if selection is at the beniggnningnngnnngggg of the list
+                    (selected_index == 0)
+                        .then_some(self.items.len() - 1) // If so, cycle back to last
+                        .unwrap_or(selected_index.saturating_sub(1)) // else, decrement the selection
+                })
+                .unwrap_or(0); // If no index is selected previously, select 0th index
+            self.state.select(Some(select_previous));
+        }
     }
 
     pub fn unselect(&mut self) {
